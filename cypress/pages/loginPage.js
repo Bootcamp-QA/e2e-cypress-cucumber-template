0e8f2a3fcb11890071c50c6a0114f63a8ec4c400
Bootcamp QA <mybootcampqa@gmail.com>
2024-06-07 13:24:01 +0200

    update

---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,29 @@
+class LoginPage {
+  elements = {
+    userInput: () => cy.get("#username").first(),
+    passwordInput: () => cy.get('#password').first(),
+    sendButton: () => cy.get('#loginbtn'),
+    successMessage: () => cy.get('.usertext'),
+    errorMessage: () => cy.get('.alert-danger')
+  };
+
+  typeUser(user) {
+    this.elements.userInput().type(user);
+  }
+
+  typePassword(password) {
+    this.elements.passwordInput().type(password);
+  }
+
+  sendForm() {
+    this.elements.sendButton().first().click();
+  }
+
+
+  fillForm(user,password){
+    this.typeUser(user);
+    this.typePassword(password);
+  }
+}
+
+export const loginPage = new LoginPage();
